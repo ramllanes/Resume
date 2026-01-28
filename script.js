c129f7f880176e38ed4e2022f7948e6a572c9862
Ram <ramil01llanes124@gmail.com>
2026-01-29 00:11:18 +0800

    resume not done

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function Weakness() {
+
+    const WeakInfo = document.querySelector('.display-description');
+    const Content = document.querySelector('.Content');
+    const profile = document.querySelectorAll('.prof');
+    const status = document.getElementById('status')
+    const originalStatus = status.textContent;
+
+    profile.forEach(prof => {
+        prof.addEventListener('mouseenter', () => {
+            WeakInfo.style.display = 'block';
+            Content.style.display = 'none';
+            
+            status.textContent = "weakness";
+
+        });
+
+        prof.addEventListener('mouseleave', () => {
+            WeakInfo.style.display = 'none';
+            Content.style.display = 'block';
+
+            status.textContent = originalStatus;
+        });
+    });
+};
+
+Weakness();
