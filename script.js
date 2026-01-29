34ff29ddf8c39ca22901784eb5ac1400a9ac5315
Ram <ramil01llanes124@gmail.com>
2026-01-29 15:19:25 +0800

    success

--- script.js
+++ script.js
@@ -3,27 +3,74 @@ function Weakness() {
 
     const WeakInfo = document.querySelector('.display-description');
     const Content = document.querySelector('.Content');
-    const profile = document.querySelectorAll('.prof');
+    const profile = document.querySelector('.prof1');
     const status = document.getElementById('status')
+    const profile2 = document.querySelector('.prof2');
+     const profile3 = document.querySelector('.prof3');
     const originalStatus = status.textContent;
+    
 
-    profile.forEach(prof => {
-        prof.addEventListener('mouseenter', () => {
+    // profile.forEach(prof => {
+    //     prof.addEventListener('mouseenter', () => {
+    //         WeakInfo.style.display = 'block';
+    //         Content.style.display = 'none';
+            
+    //         status.textContent = "weakness";
+            
+
+    //     });
+
+    profile.addEventListener('mouseenter', () => {
+
             WeakInfo.style.display = 'block';
             Content.style.display = 'none';
             
             status.textContent = "weakness";
+    })    
 
-        });
-
-        prof.addEventListener('mouseleave', () => {
+        profile.addEventListener('mouseleave', () => {
             WeakInfo.style.display = 'none';
             Content.style.display = 'block';
             
 
             status.textContent = originalStatus;
         });
-    });
-};
+
+     profile2.addEventListener('mouseenter', () => {
+
+            WeakInfo.style.display = 'block';
+            Content.style.display = 'none';
+            
+            status.textContent = "Strength";
+    })    
+
+        profile2.addEventListener('mouseleave', () => {
+            WeakInfo.style.display = 'none';
+            Content.style.display = 'block';
+            
+
+            status.textContent = originalStatus;
+        });
+        
+        
+     profile3.addEventListener('mouseenter', () => {
+
+            WeakInfo.style.display = 'block';
+            Content.style.display = 'none';
+            
+            status.textContent = "Goal";
+    })    
+
+        profile3.addEventListener('mouseleave', () => {
+            WeakInfo.style.display = 'none';
+            Content.style.display = 'block';
+            
+
+            status.textContent = originalStatus;
+        });    
+    };
+
+
+
 
 Weakness();
